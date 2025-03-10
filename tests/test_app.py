412432605786ec790b16554f66ec6ab00b3ff810
joshiaman112 <joshiaman112@gmail.com>
2025-03-10 18:37:05 -0400

    Inital Commit

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+import pytest
+
+from myapp.app import multiply_by_two, divide_by_two
+
+
+@pytest.fixture
+def numbers():
+    a = 10
+    b = 20
+    return [a,b]
+
+
+class TestApp:
+    def test_multiplication(self, numbers):
+        res = multiply_by_two(numbers[0])
+        assert res == numbers[1]
+
+    def test_division(self, numbers):
+        res = divide_by_two(numbers[1])
+        assert res == numbers[0]
+
+    def test_student_id(self):
+        assert multiply_by_two(80) == 160
